c15afe09f58de5fd67f04039010d1a33fd76560c
ahart1 <arhart555@gmail.com>
2017-06-27 15:59:18 -0400

    This almost works, but I can't figure out how to move the plotted image or make it smaller

--- ImportCustomIcons.R
+++ ImportCustomIcons.R
@@ -1,45 +1,59 @@
 # This script reads in icons from jpeg files
 
 library(ggplot2)
-library(RCurl)
-library(jpeg)
-library(grid)
-
-# Load Icon jpeg 
-# NOOOO HerringResource <- readJPEG("/Users/ahart2/Downloads/HerringResource.jpeg")
-# NOOOOO HerringResource <- readJPEG("HerringResource.jpeg")
-
+# library(RCurl)
+# library(jpeg)
+# library(grid)
 
 
 
 library(png)
 library(grid)
-# NOOOO HerringResource2 <- readPNG(system.file("img", "HerringResourcePNG.png", package="png"))
+library(graphics)
+
+setwd("/Users/ahart2/Research/MSE_Graphics/Icons")
 
 # This reads in the .png file
-HerringResource2 <- readPNG( "HerringResourcePNG.png")
-
-# Crop
-HerringResource2 <- HerringResource2[100:200, 50,]
+HerringResource2 <- readPNG( "HerringResource.png")
 
 # Turn image into a raster grob
-HerringResource2 <- rasterGrob(HerringResource2)
+#HerringResource2 <- rasterGrob(HerringResource2)
+
+
+Try <- layout(matrix(c(1,2,3,4),2,2))
+layout.show(Try)
+
+#par(mar=c(0,0,0,0))
+#plot(1,1,type="n", axes=FALSE, ann=FALSE)
+
+plot(1,1, axes=FALSE, ann=FALSE)
+rasterImage(HerringResource2,2,0.10,0.1,1)
+rasterImage(HerringResource2,01,0,01,1)
+grid.points(HerringResource2)
+
+
+# Crop
+#HerringResource2 <- HerringResource2[100:200, 50,]
+
+
+
+
+# plot(HerringResource2)this breaks things
 
 # Make the  white background transparent
 HerringResource2$raster[HerringResource2=="#FFFFFF"] <- "#FFFFFF00"
 
 # Make white layout space
-layout(matrix(2,2))
+
+#rasterImage(HerringResource2,1,1,1,1)
+# annotation_custom(HerringResource2) # this doesn't work, it breaks
 
 # Plot image as point marker using annotation_custom
-annotation_custom()
+# annotation_custom()
 
 
 Test <- readPNG(system.file("img", "Rlogo.png", package="png"))
 FixTest <- rasterGrob(Test, interpolate=FALSE)
-qplot(1:10, 1:10, geom="blank") + 
-  annotation_custom(FixTest, xmin=1, xmax=3, ymin=1, ymax=3) +  
-  geom_point()
 
 
 
