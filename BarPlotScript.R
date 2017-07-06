3e7129484cb4656960d55e7c7263ef4d5e5be148
ahart1 <arhart555@gmail.com>
2017-07-06 17:32:07 -0400

    Minor corrections made

--- BarPlotScript.R
+++ BarPlotScript.R
@@ -29,13 +29,13 @@ SinglePerfMetricPlots <- function(Data=NULL, ylab=NULL, main=NULL, PlotType=NULL
             # PlotType=="1_OperatingModel_MultipleControlRule"
                  # OperatingModel: The name of the operating model associated with data
                  # PlotColor: Vector of colors for plot                 
-            # PlotType=="Multipanel_1_OperatingModel_MultipleControlRule"
+            # PlotType=="MultiPanel_1_OperatingModel_MultipleControlRule"
                  # OperatingModelVector: A vector of operating model names
                  # PlotColor: Vector of colors for plot
             # PlotType=="1_ControlRule_MultipleOperatingModel"
                  # ControlRule: The name of the control rule associated with data
                  # PlotColor: Vector of colors for plot
-            # PlotType=="Multipanel_1_ControlRule_MultipleOperatintModel"
+            # PlotType=="MultiPanel_1_ControlRule_MultipleOperatingModel"
                  # ControlRuleVector: A vector of control rule names
                  # PlotColor: Vector of colors for plot
             # PlotType=="Y_MultipleOperatingModel_X_MultipleControlRule"
@@ -50,7 +50,7 @@ SinglePerfMetricPlots <- function(Data=NULL, ylab=NULL, main=NULL, PlotType=NULL
   # This turns data passed as extra arguments into a list for graphs to use
   ExtraArguments <- list(...) 
   
-  if(PlotType=="1_OperatingModel_MultipleControlRule"){
+  if(PlotType=="1_OperatingModel_MultipleControlRule"){ # ????????? not tested yet
     # 1 Operating Model, Multiple Control Rules
     barplot(height=Data[ExtraArguments$OperatingModel,], 
             width= rep(1,ncol(Data)), 
@@ -59,15 +59,16 @@ SinglePerfMetricPlots <- function(Data=NULL, ylab=NULL, main=NULL, PlotType=NULL
             col=ExtraArguments$PlotColor, 
             xlab="Control Rule", 
             ylab=ylab,
-            main=paste(main, "for", ExtraArguments$OperatingModel, sep=" "),
+            main=paste(main, "\n", "for", ExtraArguments$OperatingModel, sep=" "),
             axes=TRUE, 
             cex.axis=1, 
             cex.names=1, 
             offset=0)
-  } else if(PlotType=="Multipanel_1_OperatingModel_MultipleConrolRule"){
+  } else if(PlotType=="MultiPanel_1_OperatingModel_MultipleControlRule"){
     # Multipanel 1 Operating Model, Multiple Control Rules
     par(mfrow = c(4,3), mar=c(2,2,2,2))
-        for(plot in 1:length(ExtraArguments$OperatingModelVector)){
+    # For each operating model make barplot of values under different control rules
+        for(plot in 1:length(ExtraArguments$OperatingModelVector)){ 
           barplot(height=Data[plot,], 
                   width= rep(1,ncol(Data)), 
                   space=1, 
@@ -75,13 +76,13 @@ SinglePerfMetricPlots <- function(Data=NULL, ylab=NULL, main=NULL, PlotType=NULL
                   col=ExtraArguments$PlotColor, 
                   xlab="Control Rule", 
                   ylab=ylab,
-                  main=paste(main, "for", ExtraArguments$OperatingModel[plot], sep=" "),
+                  main=paste(main, "\n", "for", ExtraArguments$OperatingModel[plot], sep=" "),
                   axes=TRUE, 
                   cex.axis=1, 
                   cex.names=1, 
                   offset=0)  
         }
-  } else if(PlotType=="1_ControlRule_MultipleOperatingModel"){
+  } else if(PlotType=="1_ControlRule_MultipleOperatingModel"){ # ?????????? not tested yet
     # 1 Control Rule , Multiple Operating Models
     barplot(height=Data[,ExtraArguments$ControlRule], 
             width= rep(1,ncol(Data)), 
@@ -90,24 +91,24 @@ SinglePerfMetricPlots <- function(Data=NULL, ylab=NULL, main=NULL, PlotType=NULL
             col=ExtraArguments$PlotColor, 
             xlab="Operating Model", 
             ylab=ylab,
-            main=paste(main, "for", ExtraArguments$ControlRule, sep=" "), 
+            main=paste(main, "\n", "for", ExtraArguments$ControlRule, sep=" "), 
             axes=TRUE, 
             cex.axis=1, 
             cex.names=1, 
             offset=0)
-  } else if(PlotType=="MultiPanel_1_ControlRule_MultipleOperatingModel"){
+  } else if(PlotType=="MultiPanel_1_ControlRule_MultipleOperatingModel"){ 
     # Multipanel 1 Control Rule, Multiple Operating Models
-      par(mfrow=c(ncol(Data),3, mar=c(2,2,2,2)))
-        
+      par(mfrow = c(4,3), mar=c(2,2,2,2))
+    # For each control rule make barplot of values under different operating models
         for(plot in 1:length(ExtraArguments$ControlRuleVectors)){
-          barplot(height=Data[plot,], 
+          barplot(height=Data[,plot], 
                   width= rep(1,ncol(Data)), 
                   space=1, 
                   horiz=FALSE, 
                   col=ExtraArguments$PlotColor, 
                   xlab="Operating Model", 
                   ylab=ylab,
-                  main=paste(main, "for", ExtraArguments$ControlRule[plot], sep=" "), 
+                  main=paste(main, "\n","for", ExtraArguments$ControlRule[plot], sep=" "), 
                   axes=TRUE, 
                   cex.axis=1, 
                   cex.names=1, 
