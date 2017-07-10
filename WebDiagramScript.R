261d1c3746530d4dbde85244466780843150286f
ahart1 <arhart555@gmail.com>
2017-07-10 13:48:19 -0400

    Changes added to allow data scaling

--- WebDiagramScript.R
+++ WebDiagramScript.R
@@ -6,8 +6,12 @@ plot_indicators <- function(ind,
                             legend_labels=paste("Scenario ",1:3,sep=""),
                             axis_labels=1:8,
                             plotfile='indicator_plot.pdf',
-                            
+                            MainTitle=NULL,
                             standardized=FALSE, autodetectAxisLimits=FALSE, ...) {
+  # Args:
+       # MainTitle: Adds a main title to the plot
+       # all other arguments from: https://github.com/r4atlantis/common_scenarios_analysis/blob/master/R/plot_indicators.R
+  
   #throw warning if Isaac forgets to use a color vector the length of his scenarios
   if (length(colvec)<nrow(ind)) {
     print("YOU SUPPLIED A COLOR VECTOR THAT IS SHORTER THAN THE NUMBER OF SCNEARIOS")
@@ -15,19 +19,46 @@ plot_indicators <- function(ind,
     stop("Stopped function plot_indicators for your own sanity")
   }
   
+  # Rescale data for performance metrics ???????? still unfinished
+  Scaled_ind <- NULL
   
-  aa2 <- ind
-  #make all in zero to max(ind), can add code here to retain 0->1 for proportion indicators.
+  for(NCol in 1:ncol(ind)){
+    if(colnames(ind)[NCol] == "Yield.Relative.to.MSY"){
+      # No data scaling necessary
+      Scaled_ind <- cbind(Scaled_ind, ind[ ,NCol])
+      
+    } else if(colnames(ind)[NCol] == "Probability.of.Overfished" | colnames(ind)[NCol] == "Prop.Year.Closure.Occurs"){
+      # Scale probability data to: 1-Data
+      Scaled_ind <- cbind(Scaled_ind, 1-ind[ ,NCol])
+
+    } else if(colnames(ind)[NCol] == "Interannual.Variation.in.Yield"){
+      # Scale data to: 1/Data
+      Scaled_ind <- cbind(Scaled_ind, 1/ind[ ,NCol])
+    }  
+  }
+  # print(ind[,1])   # Check data format
+  # print(1/ind[,2])
+  # print(1-ind[,3])
+  # print(1-ind[,4])
+  # print(Scaled_ind)
+  
+  
+  
+  # Set aa2 equal to data
+  aa2 <- Scaled_ind
+  #make all in zero to max(Scaled_ind), can add code here to retain 0->1 for proportion indicators.
   aa1 <- rep(1,ncol(aa2))
   aa1 <- apply(aa2,2,max,na.rm=TRUE)
-  aa0 <- aa1 # max value
-  aa0[] <-0 # zero value
+  # Set max value for axes
+  aa0 <- aa1 
+  # Set min value (often 0) for axes
+  aa0[] <-0 
   #make MTL indicators minimum of 3
-  pick <- grep("Mtl",names(ind)[-1])
+  pick <- grep("Mtl",names(Scaled_ind)[-1])
   aa0[pick] <- 2
   
   # IK hack:
-  numOf0pt5Segments <- ceiling(max(ind,na.rm=TRUE)/0.5)
+  numOf0pt5Segments <- ceiling(max(Scaled_ind,na.rm=TRUE)/0.5)
   
   if (standardized) {
     aa1[] <- 2.0
@@ -44,7 +75,7 @@ plot_indicators <- function(ind,
   
   #combine to 
   new_dat <- as.data.frame(rbind(aa1,aa0,aa2))
-  lwd_use <- rep(4,nrow(ind))
+  lwd_use <- rep(4,nrow(Scaled_ind))
   col_use <- colvec
   if (standardized) {
     new_dat <- as.data.frame(rbind(aa1,aa0,aa3,aa2))
@@ -55,8 +86,8 @@ plot_indicators <- function(ind,
   #colnames(new_dat) <- c("Biomass","Catch","Cat/Bio","Dem:Pel","Birds & Seals","MTL Catch","Catch/PP","PropOF","Landed Value")
   #axis_labels <- names(new_dat)
   axis_labels <- rep(axis_labels,length=ncol(new_dat))
-  #legend_labels <- ind[,1]
-  legend_labels <- rep(legend_labels,length=nrow(ind))
+  #legend_labels <- Scaled_ind[,1]
+  legend_labels <- rep(legend_labels,length=nrow(Scaled_ind))
   
   if (autodetectAxisLimits)
   {
@@ -64,11 +95,11 @@ plot_indicators <- function(ind,
   }
   
   png(file=plotfile)
-  par(mar=c(0,0,3,0),oma=c(0,0,0,0))
+
+  par(mar=c(0,3,3,0), oma=c(0,0,0,0), xpd=TRUE)
   
   
-  
-  if (autodetectAxisLimits)
+  if (autodetectAxisLimits) # Set axes labels automatically when autodetectAxisLimits = TRUE
   {
     fmsb::radarchart(new_dat,pty=32,plwd=lwd_use,cglcol=gray(0.1),xlim=c(-1.5,2),
                      ylim=c(-1.5,1.5),pcol=col_use,plty=1,vlabels=axis_labels,axistype=4,seg= numOf0pt5Segments , caxislabels = as.character(seq(0, 0.5*numOf0pt5Segments ,0.5)))  #seg=5) #
@@ -77,49 +108,35 @@ plot_indicators <- function(ind,
     
     fmsb::radarchart(new_dat,pty=32,plwd=lwd_use,cglcol=gray(0.1),xlim=c(-1.5,2),
                      ylim=c(-1.5,1.5),pcol=col_use,plty=1,vlabels=axis_labels,axistype=4,seg=4, caxislabels=c("0","0.5","1","1.5","2"))
-    
-    
   }
+  
+  # Add main title to graph
+  text(0,1.7,labels=MainTitle, cex=1.5)
   
   write.csv(new_dat, file = paste(plotfile, ".csv"))              
   
-  #text(0.07,0.065,"-1",cex=2)
-  #text(0.07,0.57,"0",cex=2)
-  #text(0.07,1.035,"1",cex=2)
+  # Add legend
+  legend(0.7,1.6,legend=legend_labels,lwd=3,col=colvec,cex=1,bty='n')
   
-  
-  # fmsb::radarchart(new_dat,pty=32,plwd=lwd_use,cglcol=gray(0.1),xlim=c(-1.5,2),
-  #                   ylim=c(-1.5,1.5),pcol=col_use,plty=1,vlabels=axis_labels,caxislabels=c("-1","0","1"),axistype=1)
-  
-  
-  #  fmsb::radarchart(new_dat,pty=32,plwd=lwd_use,cglcol=gray(0.1),xlim=c(-1.5,2),
-  #                   ylim=c(-1.5,1.5),pcol=col_use,plty=1,vlabels=axis_labels,axistype = 1)
-  
-  
-  
-  # playing with raphaelcode: fmsb::radarchart(new_dat,pty=32,plwd=lwd_use,cglcol=gray(0.1),
-  #            pcol=col_use,plty=1,vlabels=axis_labels,axistype=0, seg=2,centerzero=T ,caxislabels=c("-1","0","1"))                   
-  
-  legend(0.7,1.6,legend=legend_labels,lwd=3,col=colvec,cex=0.8,bty='n')
-  #mtext(text="(a) Mortality scenarios",side=3,adj=0,line=0,cex=1.5)
   dev.off()
 }
 
 
-WebDiagramPlots <- function(Data=NULL, PlotColor = NULL, LegendLabels = NULL, AxisLabels=NULL, OutputFileName=NULL){
+WebDiagramPlots <- function(Data=NULL, PlotColor = NULL, LegendLabels = NULL, AxisLabels=NULL, OutputFileName=NULL, MainTitle=NULL){
   # Read in data and produce web diagram plot
   # plot_indicators function from: https://github.com/r4atlantis/common_scenarios_analysis/blob/master/R/plot_indicators.R
   # Minor alterations were made to produce clean graphic
   
   # Args:
-  # Data: Name of data file
-  # Should be a table with a first colum containing scenario information (Control rule or operating model names), and one column for each performance metric
-  # PlotColor: Vector of colors, must be same length as nrow(Data)
-  # LegendLabels: Vector of labels for legend (should match first column of Data)
-  # AxisLabels: Vector of labels for outside of web diagram (should match columnames, excluding first column containing senario names)
-  # OutputFileName: Name of output file
+       # Data: Name of data file
+       # Should be a table with a first colum containing scenario information (Control rule or operating model names), and one column for each performance metric
+       # PlotColor: Vector of colors, must be same length as nrow(Data)
+       # LegendLabels: Vector of labels for legend (should match first column of Data)
+       # AxisLabels: Vector of labels for outside of web diagram (should match columnames, excluding first column containing senario names)
+       # OutputFileName: String containing name of output file
+       # MainTitle: String containing title for web/radar plot
   # Returns:
-  # PNG file with web diagram for a single OM or CR depending on 
+       # PNG file with web diagram for a single OM or CR depending on 
   
   library(fmsb)
   
@@ -133,7 +150,8 @@ WebDiagramPlots <- function(Data=NULL, PlotColor = NULL, LegendLabels = NULL, Ax
                   axis_labels = AxisLabels,
                   plotfile = OutputFileName,
                   standardized = FALSE, 
-                  autodetectAxisLimits = FALSE)
+                  autodetectAxisLimits = TRUE,
+                  MainTitle = MainTitle)
 }
 
 
