7a7a3749e6ae0389cac92f7cb1212b5cdbe456c0
ahart1 <arhart555@gmail.com>
2017-08-17 14:51:08 -0400

    Set up MakePerfMetGraphicDecisionTable function to make tables for individual performance metrics across all operating models and all control rules. It adds a final summary row, although the y-axis for plots in this row is not properly labeled yet.  The default number of rows will also need to change

--- DecisionTableFunctions.R
+++ DecisionTableFunctions.R
@@ -145,7 +145,7 @@ MakeGraphicDecisionTable <- function(OutputDirectory=NULL, Title=NULL, IconList=
     
 
       for(i in 1:ncol(ExtraArguments$VerticalBarData)){
-        print(rownames(ExtraArguments$VerticalBarData))
+        # print(rownames(ExtraArguments$VerticalBarData))
         if((rownames(ExtraArguments$VerticalBarData)[row]=="Tuna Weight Status") |
           (rownames(ExtraArguments$VerticalBarData)[row]=="Prop Year Good Dogfish Biomass") |
            (rownames(ExtraArguments$VerticalBarData)[row]=="Yield") |
@@ -243,6 +243,250 @@ MakeGraphicDecisionTable <- function(OutputDirectory=NULL, Title=NULL, IconList=
 # }
 
 ##################################################################################################################
+# This function is what I used to make decision tables for individual performance metrics, it is separate simply for ease of use but should eventually be combined with the above
+
+MakePerfMetGraphicDecisionTable <- function(OutputDirectory=NULL, Title=NULL, IconList=NULL, RowCategoryName=NULL, RowNames=NULL, 
+                                     ColumnCategoryName=NULL, ColumnNames=NULL,
+                                     GraphicLayout=GraphicLayoutDefault, GraphicNRow=8, GraphicNCol=4, 
+                                     GraphicHeights=GraphicHeightsDefault, GraphicWidths=GraphicWidthsDefault, PlotOrder=NULL, OutputFileName=NULL, ...){
+  # Args:
+       # OutputDirectory: String containing the full path name of folder to store output
+       # Title: A string containing the title for the decision table graphic
+       # IconList: A vector of icons names to be printed right of the Title, number can not exceed (GraphicNCol - 4), default is empty
+       # RowCategoryName: A string containing the descriptive title for row names
+       # ColumnCategoryName: A string containing the descriptive title for column names
+       # RowNames: A vector of row names, each name should be a string in "", last item should be "Summary" or something to that effect as the last row will be a sum of each column to use as a ranking across operating models
+       # ColumnNames: A vector of column names, each name should be a string in ""
+       # GraphicLayout: A vector containing information that lays out graphic structure, the matrix must have at least 4 columns and 8 rows, default=GraphicLayoutDefault
+       # GraphicNRow: Number of rows in GraphicFormat Matrix, must be at least 4
+       # GraphicNCol: Number of columns in GraphicFormat Matrix, must be at least 8
+       # GraphicHeights: A vector containing row heights, must be same length as GraphicNRow
+       # GraphicWidths: A vector containing column widths, must be same length as GraphicNCol
+       # OutputFileName: A string contining the output file name
+       # PlotOrder: A vector of strings containing names of the plots to produce options are listed below with associated arguments, PlotOrder must be equal in length of GraphicNCol-1
+       # "SingleColumn_VerticalBarplot"
+       # VerticalBarData: List of vectors or matricies containing data to plot in vertical barplot, number of items in list must be equal to length of RowNames
+       # Each item in the list will be plotted in an individual graph
+       # VerticalBarWidths vector of bar widths
+       # VerticalBarColors: 1 or more colors for plot
+       # VerticalBarXLabel: labels x axis
+       # VerticalBarYLabel: labels y axis
+       # VerticalBarAxes determines if axes plotted, must be TRUE/FALSE
+  # Returns:
+       # A ploted decision table with customized graphics
+  
+  png(filename = paste(OutputDirectory, paste(OutputFileName, ".png", sep=""), sep="/"), width=500, height=800)
+  
+  # These set up the default format
+  GraphicLayoutDefault <- c( 1, 1, 1, 1, 2, # First four grid spaces must be assigned 1 as this is where the title will be plotted
+                             3, 3, 3, 3, 3, # 2 is empty grid to balance table appearance
+                             4, 5, 5, 5, 2,
+                             4, 6, 6, 6, 6,
+                             4, 7, 8, 9, 2,
+                             10,10,10,10,10,
+                             11,12,13,14, 2,
+                             15,15,15,15,15)
+  GraphicWidthsDefault <- c(rep(1, GraphicNCol-1), 0.5)
+  GraphicHeightsDefault <- rep(c(1,0.25), GraphicNRow/2)
+  
+  # Produce and display GraphicFormat
+  GraphicFormat <- layout(matrix(GraphicLayout, nrow = GraphicNRow, ncol = GraphicNCol, byrow = TRUE), widths = GraphicWidths, heights = GraphicHeights)
+  layout.show(GraphicFormat)
+  lcm(10)
+  
+  ##### Set up title and headers for table #####
+  # Title
+  par(mar=c(0,0,0,0))
+  plot(1,1,type="n", axes=FALSE, ann=FALSE)
+  text(1,1,labels=c(Title), cex=2.5)
+  
+  # Optional printed icons to right of title, may not exceed (GraphicNCol - 4)
+  # need a way to say if not all empty spaces used print empty, if more than empty in icon list stop printing ??????
+  library(raster)
+  library(png)
+  for(icon in 1:length(IconList)){
+    IconImage <- readPNG(paste(IconList[icon], ".png", sep=""))
+    plot(1,1, axes=FALSE, ann=FALSE) # Sets up empty plot
+    lim <- par() # Gets boundaries of plot space
+    rasterImage(IconImage, lim$usr[1], lim$usr[3], lim$usr[2], lim$usr[4]) # Plots raster image in plot space bounded by lim
+  }
+  # Need the line below for standard graphic 
+  if(length(IconList) < (GraphicNCol-5)){ # This fills remaining slots in first row with empty plots if fewer icons than slots
+    for(empty in 1:(GraphicNCol-5-length(IconList))){
+      par(mar=c(0,0,0,0))
+      plot(1,1,type="n", axes=FALSE, ann=FALSE)
+      print("Empty Plot Icon" )
+    }
+  }
+  
+  # Plot empty space on right side of graph
+  plot(1,1,type="n", axes=FALSE, ann=FALSE)
+  
+  # Plot first horizontal division
+  par(mar=c(0,0.5,0,0.5))
+  plot(1,1,type="n", axes=FALSE, ann=FALSE)
+  abline(h=1, col="black", lwd=2)
+  
+  # RowCategoryName
+  par(mar=c(0,0,0,0))
+  plot(1,1,type="n", axes=FALSE, ann=FALSE)
+  text(1,1,labels=c(RowCategoryName), cex=2) 
+  
+  # ColumnCategoryName
+  par(mar=c(0,0,0,0))
+  plot(1,1,type="n", axes=FALSE, ann=FALSE)
+  text(1,1,labels=c(ColumnCategoryName), cex=2) 
+  
+  # Plot horizontal division line
+  par(mar=c(0,0.5,0,0.5))
+  plot(1,1,type="n", axes=FALSE, ann=FALSE)
+  abline(h=1, col="black", lwd=1)
+  
+  # Plot ColumnNames
+  for(Name in 1:length(ColumnNames)){
+    plot(1,1,type = "n", axes = FALSE, ann = FALSE)
+    text(1,1, labels = ColumnNames[Name], cex=2)
+  }
+  
+  ##### Repeating information in the table #####
+  for(row in 1:(length(RowNames)-1)){
+    # Plot horizontal division line
+    par(mar=c(0,0.5,0,0.5))
+    plot(1,1,type="n", axes=FALSE, ann=FALSE)
+    abline(h=1, col="black", lwd=1)
+    
+    # Plot RowName
+    plot(1,1,type="n", axes=FALSE, ann=FALSE)
+    text(1,1,labels=RowNames[row], cex=2)
+    
+    # Plot graphs specified by PlotOrder
+    ExtraArguments <- list(...) # This turns data passed as extra arguments into a list for graphs to use
+    
+    # Determine color rank
+    for(i in 1:ncol(ExtraArguments$VerticalBarData)){
+      # print(rownames(ExtraArguments$VerticalBarData))
+      if((Title =="Tuna Weight Status") |
+         (Title =="Prop Year Good Dogfish Biomass") |
+         (Title =="Yield") |
+         (Title =="Yield Relative to MSY") |
+         (Title =="Net Revenue for Herring") |
+         (Title =="Prop Year Tern Production > 1") |
+         (Title =="SSB Relative to Unfished Biomass")){
+        
+        
+        # Rank from high to low
+        Rank <- rank(ExtraArguments$VerticalBarData[row, ]) # will not be produced if a rowname does not match something in the if statement (returns Rank not found error)
+        
+        
+      } else if((Title =="Prop Year Biomass < Bmsy") |
+                (Title =="Probability of Overfished B < 0.5Bmsy") |
+                (Title =="Prop Year Overfishing Occurs F > Fmsy") |
+                (Title =="Prop Year Closure Occurs") |
+                (Title =="Interannual Variation in Yield")){
+        
+        # Rank from low to high
+        Rank <- rank(-ExtraArguments$VerticalBarData[row,])
+      }
+      
+      Colors <- ExtraArguments$VerticalBarColors
+      
+      #print(Rank)
+      #print(ExtraArguments$VerticalBarData[row,])
+      par(mar=c(2,3,0,0), xpd=TRUE) # /?????????? probably need to adjust
+      barplot(height=ExtraArguments$VerticalBarData[row,i], 
+              width=ExtraArguments$VerticalBarWidths, 
+              space=0, 
+              horiz=FALSE, 
+              col=Colors[Rank[i]],
+              #col=ExtraArguments$VerticalBarColors[i], 
+              # xlab=Labels,
+              # ylab=ExtraArguments$VerticalBarYLabel,
+              ylim=c(0,1.1*max(ExtraArguments$VerticalBarData[row,])),
+              # axes=ExtraArguments$VerticalBarAxes, 
+              cex.axis=1, 
+              cex.names=1, 
+              offset=0)
+      
+      Labels <- round(ExtraArguments$VerticalBarData[row,i], digits=2)
+      # text(0.23,-0.13,labels=paste(Labels), cex=1.5)
+      # text (0.23,-0.5, labels = Labels, cex=1.5)
+      mtext(Labels, side=1, cex=1.2, line=1)
+    }
+  }
+  
+  ##### Last Row Summary #####
+  # Plot horizontal division line
+  par(mar=c(0,0.5,0,0.5))
+  plot(1,1,type="n", axes=FALSE, ann=FALSE)
+  abline(h=1, col="black", lwd=1)
+  
+  # Plot RowName
+  plot(1,1,type="n", axes=FALSE, ann=FALSE)
+  text(1,1,labels=RowNames[length(RowNames)], cex=2) # plot last rowname
+  
+  
+  SummaryData_Sum <- colSums(ExtraArguments$VerticalBarData)
+  # print(SummaryData_Sum)
+  
+  # Determine color rank for summary data
+  for(i in 1:length(SummaryData_Sum)){
+    # print(rownames(ExtraArguments$VerticalBarData))
+    if((Title =="Tuna Weight Status") |
+       (Title =="Prop Year Good Dogfish Biomass") |
+       (Title =="Yield") |
+       (Title =="Yield Relative to MSY") |
+       (Title =="Net Revenue for Herring") |
+       (Title =="Prop Year Tern Production > 1") |
+       (Title =="SSB Relative to Unfished Biomass")){
+      
+      # Rank from high to low
+      Rank <- rank(SummaryData_Sum) # will not be produced if a rowname does not match something in the if statement (returns Rank not found error)
+      
+    } else if((Title =="Prop Year Biomass < Bmsy") |
+              (Title =="Probability of Overfished B < 0.5Bmsy") |
+              (Title =="Prop Year Overfishing Occurs F > Fmsy") |
+              (Title =="Prop Year Closure Occurs") |
+              (Title =="Interannual Variation in Yield")){
+      
+      # Rank from low to high
+      Rank <- rank(-SummaryData_Sum)
+    }
+    
+    Colors <- ExtraArguments$VerticalBarColors
+    
+    #print(Rank)
+    #print(ExtraArguments$VerticalBarData[row,])
+    par(mar=c(2,3,0,0), xpd=TRUE) # /?????????? probably need to adjust
+    barplot(height=SummaryData_Sum[i], 
+            width=ExtraArguments$VerticalBarWidths, 
+            space=0, 
+            horiz=FALSE, 
+            col=Colors[Rank[i]],
+            #col=ExtraArguments$VerticalBarColors[i], 
+            # xlab=Labels,
+            # ylab=ExtraArguments$VerticalBarYLabel,
+            ylim=c(0,(1.1*max(SummaryData_Sum))),  # Scaling of these axes are standardized across the row but not across the entire column (summary axes are larger)
+            ##### The above line is not setting max axis correctly (should be more than 4.5 in this example but is instead set to 2) ????
+            # axes=ExtraArguments$VerticalBarAxes, 
+            cex.axis=1, 
+            cex.names=1, 
+            offset=0)
+    print(max(SummaryData_Sum))
+    Labels <- round(SummaryData_Sum[i], digits=2)
+    mtext(Labels, side=1, cex=1.2, line=1)
+  }
+  
+  
+  
+  
+  # Plot last horizontal division
+  par(mar=c(0,0.5,0,0.5))
+  plot(1,1,type="n", axes=FALSE, ann=FALSE)
+  abline(h=1, col="black", lwd=2)
+  
+  dev.off()
+}
+
 
 
 
